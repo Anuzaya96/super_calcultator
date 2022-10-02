0bf931dfd8ff1acde6f6f56e6d663ac0ec98cf6c
Anuzaya96 <filipciuk10@gmail.com>
2022-10-02 11:09:31 +0200

    spalvos

--- super_calculator.py
+++ super_calculator.py
@@ -41,25 +41,32 @@ frame_1 = Frame(langas)
 frame_1.grid(row=2, column=1)
 
 
-mygtukas1 = Button(frame_1, text="+",command=sudetis)
-mygtukas2 = Button(frame_1, text="-",command=atimtis)
-mygtukas3 = Button(frame_1, text="*",command=daugyba)
-mygtukas4 = Button(frame_1, text="/", command=dalyba)
+mygtukas1 = Button(frame_1, text="+", command=sudetis, bg="#ccfff5")
+mygtukas1.config(font=(12))
+
+mygtukas2 = Button(frame_1, text="-",command=atimtis, bg="#ccfff5")
+mygtukas2.config(font=(15))
+
+mygtukas3 = Button(frame_1, text="*",command=daugyba, bg="#ccfff5")
+mygtukas3.config(font=(15))
+
+mygtukas4 = Button(frame_1, text="/", command=dalyba,bg="#ccfff5")
+mygtukas3.config(font=(15))
 
 
-mygtukas1.grid(row=0, column=0, stick="wens",)
-mygtukas2.grid(row=0, column=1, stick="wens",)
-mygtukas3.grid(row=0, column=2, stick="wens",)
-mygtukas4.grid(row=0, column=3, stick="wens",)
+mygtukas1.grid(row=0, column=0)
+mygtukas2.grid(row=0, column=1)
+mygtukas3.grid(row=0, column=2)
+mygtukas4.grid(row=0, column=3)
 
 
-label_skaicius1 = Label(langas, text="iveskite pirma skaiciu:", width=25)
+label_skaicius1 = Label(langas, text="Įveskite pirmą skaičių:", width=25, bg="#80ccff")
 label_skaicius1.grid(row=0, column=0, padx=5, pady=5)
 skaicius_1 = Entry(langas)
 skaicius_1.grid(row=0, column=1, padx=5, pady=5)
 
 
-label_skaicius2 = Label(langas, text="iveskite antra skaiciu:",width=25)
+label_skaicius2 = Label(langas, text="Įveskite antrą skaičių:",width=25, bg="#80ccff")
 label_skaicius2.grid(row=1, column=0, padx=5, pady=5)
 skaicius_2 = Entry(langas)
 skaicius_2.grid(row=1, column=1, padx=5, pady=5)
@@ -67,13 +74,13 @@ skaicius_2.grid(row=1, column=1, padx=5, pady=5)
 
 label_atsakymas= Label(langas, text="Atsakymas:" )
 label_atsakymas.grid(row=3, column=1)
-rezultatas = Label(langas, text="rezultatas", width=25)
+rezultatas = Label(langas, text="Rezultatas", width=25, bg="#80ccff")
 rezultatas.grid(row=3, column=0, padx=5, pady=5)
 
-operacija = Label(langas, text="Pasirinkite matematini veiksma:",width=25)
+operacija = Label(langas, text="Pasirinkite matematinį veiksmą:",width=25,bg="#80ccff" )
 operacija.grid(row=2, column=0,padx=5, pady=5)
 
-delete = Button(langas, text="Istrnti", command=istrint)
+delete = Button(langas, text="Ištrnti", command=istrint, bg="#ff7733")
 delete.grid(row=4, column=0, padx=5, pady=5)
 
 
