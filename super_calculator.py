941b02ac99dac07fed910f57fddf4fe389e9db80
Anuzaya96 <filipciuk10@gmail.com>
2022-10-02 10:26:34 +0200

    paketimas

--- super_calculator.py
+++ super_calculator.py
@@ -1,60 +1,80 @@
-import tkinter as tk
 
-langas = tk.Tk()
-
-# rasau funkcija add_digit ir perduodu skaciaus pavydalu, ir kreipiosi i calc ir 
-# pridedu insert metoda,
-# dabar isivedineja skaiciai
-  
-def add_digit(digit):
-    calc.insert(0,digit)
+from tkinter import *
 
 
-ikonele = tk.PhotoImage(file="pict/calculator.png")
+def sudetis():
+    rezultatas = int(skaicius_1.get()) + int(skaicius_2.get())
+    label_atsakymas["text"] = rezultatas
+
+def atimtis():
+    rezultatas = int(skaicius_1.get()) - int(skaicius_2.get())
+    label_atsakymas["text"] = rezultatas
+
+def daugyba():
+    rezultatas = int(skaicius_1.get()) * int(skaicius_2.get())
+    label_atsakymas["text"] = rezultatas
+    
+def dalyba():
+    rezultatas = int(skaicius_1.get()) / int(skaicius_2.get())
+    label_atsakymas["text"] = rezultatas
+
+def istrint():
+    skaicius_1.delete(0, END)
+    skaicius_2.delete(0, END)
+    label_atsakymas["text"] = "Atsakymas:"
+
+langas = Tk()
+
+langas['bg'] = '#14066F'
+langas.title('Calculator')
+langas.geometry('350x170')
+
+ikonele = PhotoImage(file="pict/calculator.png")
 langas.iconphoto(False, ikonele)
 
-
-langas.geometry("240x250")
-langas.config(bg="#14066F")
-langas.title("Super Calculator")
-
-langas.minsize(300,400)
-langas.maxsize(500,550)
+langas.minsize(320,170)
+langas.maxsize(400,250)
 langas.resizable(True, True)
 
-# culumnspan sujungia kelias koloneles, sio atveju man reikia 3
-calc = tk.Entry(langas)
-calc.grid(row=0, column=0, columnspan=3)
 
-# stick - mygtukas issiplecia i visas  pasaulio puses stick="wens"
-# atributai padx,pady atsako uz tarpus tarp mygtuku bus po 5
-# bd darau seselius mygtuku. bd funkcija rasoma prie mygtuku
-# command= rasomas prie tk.Button lambda kuri iskviecia identiska funkcija ir + add_diggit
-# parasyti funkcija add_digit
+frame_1 = Frame(langas)
+frame_1.grid(row=2, column=1)
 
 
-tk.Button(text="1", bd=3, command= lambda : add_digit(1)).grid(row=1, column=0, stick="wens", padx=5, pady=5)
-tk.Button(text="2", bd=3, command= lambda : add_digit(2)).grid(row=1, column=1, stick="wens", padx=5, pady=5)
-tk.Button(text="3", bd=3, command= lambda : add_digit(3)).grid(row=1, column=2, stick="wens", padx=5, pady=5)
-tk.Button(text="4", bd=3, command= lambda : add_digit(3)).grid(row=2, column=0, stick="wens", padx=5, pady=5)
-tk.Button(text="5", bd=3, command= lambda : add_digit(4)).grid(row=2, column=1, stick="wens", padx=5, pady=5)
-tk.Button(text="6", bd=3, command= lambda : add_digit(5)).grid(row=2, column=2, stick="wens", padx=5, pady=5)
-tk.Button(text="7", bd=3, command= lambda : add_digit(6)).grid(row=3, column=0, stick="wens", padx=5, pady=5)
-tk.Button(text="8", bd=3, command= lambda : add_digit(7)).grid(row=3, column=1, stick="wens", padx=5, pady=5)
-tk.Button(text="9", bd=3, command= lambda : add_digit(8)).grid(row=3, column=2, stick="wens", padx=5, pady=5)
-tk.Button(text="0", bd=3, command= lambda : add_digit(9)).grid(row=4, column=0, stick="wens", padx=5, pady=5)
-tk.Button(text="0", bd=3, command= lambda : add_digit(0)).grid(row=4, column=0, stick="wens", padx=5, pady=5)
- 
-# duodu minimalu dydi kolonelei ir surasau nr
-langas.grid_columnconfigure(0,minsize=60)
-langas.grid_columnconfigure(1,minsize=60)
-langas.grid_columnconfigure(2,minsize=60)
+mygtukas1 = Button(frame_1, text="+",command=sudetis)
+mygtukas2 = Button(frame_1, text="-",command=atimtis)
+mygtukas3 = Button(frame_1, text="*",command=daugyba)
+mygtukas4 = Button(frame_1, text="/", command=dalyba)
 
-# eiliu dydi pakeiciu
-langas.grid_rowconfigure(1,minsize=60)
-langas.grid_rowconfigure(2,minsize=60)
-langas.grid_rowconfigure(3,minsize=60)
-langas.grid_rowconfigure(4,minsize=60)
+
+mygtukas1.grid(row=0, column=0, stick="wens",)
+mygtukas2.grid(row=0, column=1, stick="wens",)
+mygtukas3.grid(row=0, column=2, stick="wens",)
+mygtukas4.grid(row=0, column=3, stick="wens",)
+
+
+label_skaicius1 = Label(langas, text="iveskite pirma skaiciu:", width=25)
+label_skaicius1.grid(row=0, column=0, padx=5, pady=5)
+skaicius_1 = Entry(langas)
+skaicius_1.grid(row=0, column=1, padx=5, pady=5)
+
+
+label_skaicius2 = Label(langas, text="iveskite antra skaiciu:",width=25)
+label_skaicius2.grid(row=1, column=0, padx=5, pady=5)
+skaicius_2 = Entry(langas)
+skaicius_2.grid(row=1, column=1, padx=5, pady=5)
+
+
+label_atsakymas= Label(langas, text="Atsakymas:" )
+label_atsakymas.grid(row=3, column=1)
+rezultatas = Label(langas, text="rezultatas", width=25)
+rezultatas.grid(row=3, column=0, padx=5, pady=5)
+
+operacija = Label(langas, text="Pasirinkite matematini veiksma:",width=25)
+operacija.grid(row=2, column=0,padx=5, pady=5)
+
+delete = Button(langas, text="Istrnti", command=istrint)
+delete.grid(row=4, column=0, padx=5, pady=5)
 
 
 langas.mainloop()
---
+++ uzd.py
@@ -0,0 +1,24 @@
+# programa turi skaicioti paprastas operacijas: + - * /
+# programa turi tureti mygtukus kurie isiveda lange sudeti atimti dauginti dalinti
+# programa turi issaugoti rezultata
+
+#Padaryti langa
+# padaryti Label, Entry 
+
+
+
+
+
+
+
+
+
+
+
+
+# turi skaiciai prasideti nuo desnes
+# ivedimo langas turi prasideti per visa ilgi
+# padaryti operacijos mygtukus
+
+
+
